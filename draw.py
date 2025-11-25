42e55d2877de0acdcf606b6cb6478d4f2620db62
Nguyen The Minh <minha0pro@gmail.com>
2025-11-25 13:59:42 +0700

    update line label

--- draw.py
+++ draw.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def draw_line(x, line, color):
+def draw_line(x, line, color, prefix):
     # Generate x values
     a = line[1]
     b = line[2]
@@ -10,7 +10,7 @@ def draw_line(x, line, color):
     # Compute y values using y = (-a*x - c)/b, if b != 0
     if b != 0:
         y = (-a * x - c) / b
-        plt.plot(x, y, label=f'{a}x + {b}y + {c} = 0')
+        plt.plot(x, y, label=f'{prefix}: {a}x + {b}y + {c} = 0')
     else:
         # Vertical line case: x = -c/a
         x_line = -c / a
@@ -20,8 +20,8 @@ def draw_line(x, line, color):
 def draw_line_and_samples(f, g, samples, test_samples, size, test_size):
     # Generate x values
     axis = np.linspace(-100, 100, 400)
-    draw_line(axis, f, 'r')
-    draw_line(axis, g, 'm')
+    draw_line(axis, f, 'r', 'f')
+    draw_line(axis, g, 'm', 'g')
     xs, ys = samples
     xt, yt = test_samples 
 
